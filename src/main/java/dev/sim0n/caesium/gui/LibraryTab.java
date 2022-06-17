8ab822222ac7b0a763649e4c1e4e0cd657c8d24b
eltonsandre <eltonsandre@gmail.com>
2022-06-17 19:46:43 -0300

    Refactor GUI

--- src/main/java/dev/sim0n/caesium/gui/LibraryTab.java
+++ src/main/java/dev/sim0n/caesium/gui/LibraryTab.java
@@ -1,102 +1,120 @@
 package dev.sim0n.caesium.gui;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
 import dev.sim0n.caesium.PreRuntime;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.io.File;
-import java.util.List;
 
 public class LibraryTab extends JPanel {
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 4696970146752109196L;
+
+    private JTextField librariesField;
+    private JButton addButton;
+    private JButton removeButton;
+    private JPanel contentPanel;
+    private JList<String> libList;
+
+    protected DefaultListModel<String> dependenciesListModel;
+
+    private String currentProfile = "user.home";
 
     public LibraryTab() {
-        GridBagLayout gbl_this = new GridBagLayout();
-        gbl_this.columnWidths = new int[] { 0, 0, 0, 0, 0 };
-        gbl_this.rowHeights = new int[] { 0, 0, 0 };
-        gbl_this.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
-        gbl_this.rowWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
-        this.setLayout(gbl_this);
+        init();
+    }
 
-        JScrollPane ScrollPane = new JScrollPane();
-        GridBagConstraints gbc_ScrollPane = new GridBagConstraints();
-        gbc_ScrollPane.gridwidth = 4;
-        gbc_ScrollPane.insets = new Insets(5, 5, 5, 5);
-        gbc_ScrollPane.fill = GridBagConstraints.BOTH;
-        gbc_ScrollPane.gridx = 0;
-        gbc_ScrollPane.gridy = 0;
-        this.add(ScrollPane, gbc_ScrollPane);
+    void init() {
+        dependenciesListModel = new DefaultListModel<>();
+        PreRuntime.libraries.forEach(dependenciesListModel::addElement);
 
-        JList<String> libList = new JList<>(PreRuntime.libraries);
-        ScrollPane.setViewportView(libList);
-
-        JTextField librariesField = new JTextField();
-        GridBagConstraints gbc_librariesField = new GridBagConstraints();
-        gbc_librariesField.insets = new Insets(0, 0, 5, 5);
-        gbc_librariesField.fill = GridBagConstraints.HORIZONTAL;
-        gbc_librariesField.gridx = 1;
-        gbc_librariesField.gridy = 1;
-        this.add(librariesField, gbc_librariesField);
-        librariesField.setColumns(10);
-
-        JButton addButton = new JButton("Add");
-        GridBagConstraints gbc_addButton = new GridBagConstraints();
-        gbc_addButton.insets = new Insets(0, 0, 5, 5);
-        gbc_addButton.gridx = 2;
-        gbc_addButton.gridy = 1;
-        addButton.addActionListener((e) -> {
+        libList.setModel(dependenciesListModel);
+        addButton.addActionListener(e -> {
             if (librariesField.getText().length() > 1) {
                 String path = librariesField.getText();
-
-                File file = new File(path);
-
-                if (file.exists()) {
-                    PreRuntime.libraries.addElement(path);
-                    librariesField.setText(null);
-                } else {
-                    JOptionPane.showMessageDialog(null, "Could not locate dependency.", "Caesium Dependency",
+                if (!addDependencyPath(path)) {
+                    JOptionPane.showMessageDialog(contentPanel, "Could not locate dependency.", "Caesium Dependency",
                             JOptionPane.ERROR_MESSAGE);
                 }
             } else {
-                final JFileChooser chooser = new JFileChooser();
-                final FileNameExtensionFilter filter = new FileNameExtensionFilter("Java File", "jar");
+                final JFileChooser chooser = new JFileChooser(currentProfile);
+                final FileNameExtensionFilter filter = new FileNameExtensionFilter("Java File", "jar", "jmod");
                 chooser.setFileFilter(filter);
                 chooser.setMultiSelectionEnabled(true);
                 chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-
                 int result = chooser.showOpenDialog(librariesField);
 
                 if (result == 0) {
+                    currentProfile = chooser.getSelectedFile().getParent();
                     File[] libs = chooser.getSelectedFiles();
                     for (File lib : libs) {
-                        PreRuntime.libraries.addElement(lib.toString());
+                         addDependencyPath(lib.toString());
                     }
                 }
             }
         });
 
-        this.add(addButton, gbc_addButton);
+        removeButton.addActionListener(e ->
+                libList.getSelectedValuesList()
+                        .forEach(dependency -> {
+                            PreRuntime.libraries.remove(dependency);
+                            dependenciesListModel.removeElement(dependency);
+                        }));
+    }
 
-        JButton removeButton = new JButton("Remove");
-        GridBagConstraints gbc_removeButton = new GridBagConstraints();
-        gbc_removeButton.insets = new Insets(0, 0, 5, 5);
-        gbc_removeButton.gridx = 3;
-        gbc_removeButton.gridy = 1;
-        removeButton.addActionListener((e) -> {
-            List<String> removeList = libList.getSelectedValuesList();
-            if (removeList.isEmpty())
-                return;
-
-            for (String s : removeList) {
-                PreRuntime.libraries.removeElement(s);
+    protected boolean addDependencyPath(final String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            PreRuntime.libraries.add(path);
+            if (!dependenciesListModel.contains(path)) {
+                dependenciesListModel.addElement(path);
             }
-        });
-        this.add(removeButton, gbc_removeButton);
+            librariesField.setText(null);
+            return true;
+        }
+        return false;
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        contentPanel = new JPanel();
+        contentPanel.setLayout(new GridLayoutManager(2, 3, new Insets(0, 0, 0, 0), -1, -1));
+        librariesField = new JTextField();
+        contentPanel.add(librariesField, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        addButton = new JButton();
+        addButton.setText("Add");
+        contentPanel.add(addButton, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        removeButton = new JButton();
+        removeButton.setText("Remove");
+        contentPanel.add(removeButton, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        contentPanel.add(panel1, new GridConstraints(0, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        panel1.add(scrollPane1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        libList = new JList();
+        scrollPane1.setViewportView(libList);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return contentPanel;
     }
 
 }
